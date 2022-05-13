0c710f8032ee87a9bce0cdff9ff6d6ae0389ccd2
Citra Febriawirti <83469523+citraFebriawirti@users.noreply.github.com>
2022-05-13 21:20:29 +0700

    Add files via upload

---
+++ Pertemuan 3/JS/tipe_data.js
@@ -0,0 +1,50 @@
+/* Tipe Data adalah nilai untuk mengintegrasikan dalam javascript dari jenis tertentu */
+
+//number
+let nilai = 2;
+console.log(nilai);
+console.log(typeof(nilai));
+/*---Output : 2 dan number---*/
+
+
+let nilai1 = 10.4;
+console.log(nilai1);
+console.log(typeof(nilai1));
+/*---Output : 10.4 dan number---*/
+
+//BigIn
+const myInt = 1234567890123456789012345678901234567890;
+console.log(myInt);
+console.log(typeof(myInt));
+/*---Output : 1.2345678901234568e+39 dan number */
+
+
+//string
+let a = "Belajar Javascript!";
+console.log(a);
+console.log(typeof(a));
+/*---Output : Belajar Javascript dan string */
+
+let b = "Ada Project ada cuan";
+console.log(`Para Mentor, ${b}`);
+console.log(typeof(b));
+/*---output : Para Mentor, Ada Project ada cuan dan string */
+
+//boolean
+let x = true;
+let y = false;
+console.log(typeof(x));
+console.log(typeof(y));
+/*---Output : boolean dan boolean */
+
+let isGreater = 6 > 9;
+console.log(isGreater)
+console.log(typeof(isGreater));
+/*---Output : false dan boolean */
+
+//undefined
+let age;
+console.log(age);
+console.log(typeof(age));
+console.log(typeof(y));
+/*---Output : undefined dan undefined*/
